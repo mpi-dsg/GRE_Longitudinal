99b767f24d5cfd642d8caf4d768c6bc705fa5db3
maqamylee0 <maqamylee0@gmail.com>
2025-07-05 05:35:24 +0300

    update kanva

--- src/competitor/kanva/kanva_RS.h
+++ src/competitor/kanva/kanva_RS.h
@@ -1,10 +1,13 @@
 // #include "./src/osm/include/function.h"
 #include "./src/Kanva_RS/kanva_RS.h"
 #include "../indexInterface.h"
+#include "./src/common/util.h"
 
 template<class KEY_TYPE, class PAYLOAD_TYPE>
 class kanvaInterface : public indexInterface<KEY_TYPE, PAYLOAD_TYPE> {
 public:
+  kanvaInterface() : index(1) {} // Use the single parameter constructor with 1 thread
+  
   void init(Param *param = nullptr) {}
 
   void bulk_load(std::pair <KEY_TYPE, PAYLOAD_TYPE> *key_value, size_t num, Param *param = nullptr);
@@ -13,7 +16,7 @@ public:
 
   bool put(KEY_TYPE key, PAYLOAD_TYPE value, Param *param = nullptr);
 
-//   bool update(KEY_TYPE key, PAYLOAD_TYPE value, Param *param = nullptr);
+  bool update(KEY_TYPE key, PAYLOAD_TYPE value, Param *param = nullptr);
 
   bool remove(KEY_TYPE key, Param *param = nullptr);
 
@@ -23,7 +26,7 @@ public:
   long long memory_consumption() { return 0; }
 
 private:
-  kanva::Kanva_RS<KEY_TYPE, PAYLOAD_TYPE> index;
+  kanva_RS::Kanva_RS<KEY_TYPE, PAYLOAD_TYPE> index;
 };
 
 template<class KEY_TYPE, class PAYLOAD_TYPE>
@@ -36,13 +39,13 @@ void kanvaInterface<KEY_TYPE, PAYLOAD_TYPE>::bulk_load(std::pair <KEY_TYPE, PAYL
       key_temp.push_back(key_value[i].first);
       val_temp.push_back(key_value[i].second);
   }
-  index.train(key_temp, val_temp, 32);
+  index.train(key_temp, val_temp, 32, 0); // Add thread_id_t parameter (0)
 }
 
 template<class KEY_TYPE, class PAYLOAD_TYPE>
 bool kanvaInterface<KEY_TYPE, PAYLOAD_TYPE>::get(KEY_TYPE key, PAYLOAD_TYPE &val, Param *param) {
-  kanva::result_t res = index.find(key, val);
-  if(res == kanva::result_t::ok) {
+  kanva_RS::result_t res = index.find(key, val, 0);
+  if(res == kanva_RS::result_t::ok) {
     return true;
   }
   return false;
@@ -50,26 +53,26 @@ bool kanvaInterface<KEY_TYPE, PAYLOAD_TYPE>::get(KEY_TYPE key, PAYLOAD_TYPE &val
 
 template<class KEY_TYPE, class PAYLOAD_TYPE>
 bool kanvaInterface<KEY_TYPE, PAYLOAD_TYPE>::put(KEY_TYPE key, PAYLOAD_TYPE value, Param *param) {
-  kanva::result_t res = index.insert(key, value);
-  if(res == kanva::result_t::ok) {
+  kanva_RS::result_t res = index.insert(key, value, 0);
+  if(res == kanva_RS::result_t::ok) {
     return true;
   }
   return false;
 }
 
-// template<class KEY_TYPE, class PAYLOAD_TYPE>
-// bool finedexInterface<KEY_TYPE, PAYLOAD_TYPE>::update(KEY_TYPE key, PAYLOAD_TYPE value, Param *param) {
-//   finedex::result_t res = index.update(key, value);
-//   if(res == finedex::result_t::ok) {
-//     return true;
-//   }
-//   return false;
-// }
+template<class KEY_TYPE, class PAYLOAD_TYPE>
+bool kanvaInterface<KEY_TYPE, PAYLOAD_TYPE>::update(KEY_TYPE key, PAYLOAD_TYPE value, Param *param) {
+  kanva_RS::result_t res = index.update(key, value, 0);
+  if(res == kanva_RS::result_t::ok) {
+    return true;
+  }
+  return false;
+}
 
 template<class KEY_TYPE, class PAYLOAD_TYPE>
 bool kanvaInterface<KEY_TYPE, PAYLOAD_TYPE>::remove(KEY_TYPE key, Param *param) {
- kanva::result_t res = index.remove(key);
-  if(res == kanva::result_t::ok) {
+ kanva_RS::result_t res = index.remove(key, 0);
+  if(res == kanva_RS::result_t::ok) {
     return true;
   }
   return false;
@@ -81,6 +84,6 @@ size_t kanvaInterface<KEY_TYPE, PAYLOAD_TYPE>::scan(KEY_TYPE key_low_bound, size
                                                    Param *param) {
   std::vector<std::pair<KEY_TYPE, PAYLOAD_TYPE>> res;
   res.reserve(key_num);
-  size_t scan_size = index.scan(key_low_bound, key_num, res);
+  size_t scan_size = index.rangequery(key_low_bound, key_num, res, 0);
   return scan_size;
 }
